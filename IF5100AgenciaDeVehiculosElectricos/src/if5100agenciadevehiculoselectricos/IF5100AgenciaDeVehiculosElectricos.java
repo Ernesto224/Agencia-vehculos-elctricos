9bb6d0212840a07a746a36bc416be00d2bb881a9
Josué <josuemarin2009@hotmail.com>
2024-05-21 12:03:24 -0600

    Ventana principal, encriptacipón e inicio sesión

--- IF5100AgenciaDeVehiculosElectricos/src/if5100agenciadevehiculoselectricos/IF5100AgenciaDeVehiculosElectricos.java
+++ IF5100AgenciaDeVehiculosElectricos/src/if5100agenciadevehiculoselectricos/IF5100AgenciaDeVehiculosElectricos.java
@@ -1,5 +1,6 @@
 package if5100agenciadevehiculoselectricos;
 
+import GUI.JFInicioSesion;
 import GUI.JIFGenerarFactura;
 import GUI.JIFListarVehiculos;
 import GUI.JIFReservarProducto;
@@ -9,37 +10,8 @@ import javax.swing.JDesktopPane;
 public class IF5100AgenciaDeVehiculosElectricos {
 
     public static void main(String[] args) {
-        // Crear un nuevo marco principal
-        JFrame frame = new JFrame("Agencia de Vehículos Eléctricos");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Crear un nuevo escritorio para contener los JInternalFrames
-        JDesktopPane desktopPane = new JDesktopPane();
-
-        // Crear una instancia del JInternalFrame que quieres mostrar
-        JIFReservarProducto jIFReservarProducto = new JIFReservarProducto();
-        JIFListarVehiculos jIFListarVehiculos = new JIFListarVehiculos();
-        JIFGenerarFactura fGenerarFactura = new JIFGenerarFactura();
         
-        // Agregar el JInternalFrame al escritorio
-        desktopPane.add(jIFListarVehiculos);
-        desktopPane.add(jIFReservarProducto);
-        desktopPane.add(fGenerarFactura);
-        // Hacer visible el JInternalFrame
-        jIFReservarProducto.setVisible(true);
-        jIFListarVehiculos.setVisible(true);
-        fGenerarFactura.setVisible(true);
-        
-        // Agregar el escritorio al marco principal
-        frame.add(desktopPane);
-
-        // Establecer el tamaño del marco
-        frame.setSize(1000, 1000);
-
-        // Centrar el marco en la pantalla
-        frame.setLocationRelativeTo(null);
-
-        // Hacer visible el marco principal
-        frame.setVisible(true);
+       JFInicioSesion jFInicioSesion = new JFInicioSesion();
+       jFInicioSesion.setVisible(true);
     }
 }
