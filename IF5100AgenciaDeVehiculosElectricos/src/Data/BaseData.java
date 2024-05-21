6c8aaec914b9ff11bc218318f3eee5f7aa4d2f1a
Jesner <JESNER.MELGARA@ucr.ac.cr>
2024-05-21 16:34:10 -0600

    String de conexion arreglado

--- IF5100AgenciaDeVehiculosElectricos/src/Data/BaseData.java
+++ IF5100AgenciaDeVehiculosElectricos/src/Data/BaseData.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class BaseData {
-   private final String sqlUrl = "jdbc:sqlserver://163.178.107.10;User Id=Laboratorios;Password=TUy&)&nfC7QqQau.%278UQ24/=%;Initial Catalog=Agencia_De_Vehiculos_Electricos;TrustServerCertificate=true;";
+   private final String sqlUrl = "jdbc:sqlserver://163.178.107.10:1433;databaseName=Agencia_De_Vehiculos_Electricos;user=laboratorios;password=TUy&)&nfC7QqQau.%278UQ24/=%;encrypt=false";
     public Connection getSqlConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(sqlUrl);
         return connection;
